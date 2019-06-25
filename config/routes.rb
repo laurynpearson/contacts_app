34f325db78dd091fc3c88cf7614d5f85d9efcb7b
Lauryn <laurynpearson@gmail.com>
2019-06-24 20:58:47 -0500

    create namespaced route that displays JSON data about first contact

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/contacts" => "contacts#contact_list"
+  namespace :api do
+    get "/contacts" => "contacts#contact_list"
   #   get "/photos" => "photos#index"
   end
 end
